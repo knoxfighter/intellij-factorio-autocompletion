601545205e2b2914e0c54fd70c9856f5208dcb81
knoxfighter <knoxfighter@asdff45.de>
2019-12-19 17:45:29 +0100

    added missing ints to builtin-types

--- src/main/resources/library/builtin-types.lua
+++ src/main/resources/library/builtin-types.lua
@@ -2,7 +2,11 @@
 ---@alias double number
 ---@alias int number
 ---@alias int8 number
+---@alias int16 number
+---@alias int32 number
+---@alias int64 number
 ---@alias uint number
 ---@alias uint8 number
 ---@alias uint16 number
+---@alias uint32 number
 ---@alias uint64 number
