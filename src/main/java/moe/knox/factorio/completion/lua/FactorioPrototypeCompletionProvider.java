3b953d3bed9b3c072a69b58588af30c846f13df7
knoxfighter <knoxfighter@asdff45.de>
2019-12-19 17:46:23 +0100

    moved FactorioPrototype type guessing to its own class & added insert Completion based on type

--- src/main/java/moe/knox/factorio/completion/lua/FactorioPrototypeCompletionProvider.java
+++ src/main/java/moe/knox/factorio/completion/lua/FactorioPrototypeCompletionProvider.java
@@ -3,19 +3,17 @@ package moe.knox.factorio.completion.lua;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
 import com.tang.intellij.lua.editor.completion.LookupElementFactory;
+import com.tang.intellij.lua.editor.completion.LuaFieldLookupElement;
 import com.tang.intellij.lua.editor.completion.LuaLookupElement;
-import com.tang.intellij.lua.psi.LuaClass;
 import com.tang.intellij.lua.psi.LuaClassField;
 import com.tang.intellij.lua.psi.LuaTableExpr;
 import com.tang.intellij.lua.psi.LuaTableField;
-import com.tang.intellij.lua.psi.search.LuaShortNamesManager;
 import com.tang.intellij.lua.search.SearchContext;
-import com.tang.intellij.lua.ty.ITyClass;
+import com.tang.intellij.lua.ty.*;
+import moe.knox.factorio.FactorioPrototypeTypeGuesser;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -28,77 +26,56 @@ public class FactorioPrototypeCompletionProvider extends CompletionProvider<Comp
         if (fieldName != null) {
             // this is a subelement :)
             // TODO autocompletion for subelements
+            FactorioPrototypeTypeGuesser.guessType(parameters.getPosition());
         } else {
+            resultSet.stopHere();
+
             /// The completion for the tableFieldName
-            // get table element
-            LuaTableExpr table = PsiTreeUtil.getParentOfType(parameters.getPosition(), LuaTableExpr.class);
-            if (table != null) {
-                resultSet.stopHere();
-
-                // get additional vars
-                Project project = table.getProject();
-                PrefixMatcher prefixMatcher = resultSet.getPrefixMatcher();
-                SearchContext searchContext = SearchContext.Companion.get(project);
-
-                // find the type of this table
-                LuaTableField type = table.findField("type");
-                if (type == null) {
-                    // only complete member `type`
-                    LuaLookupElement element = new LuaLookupElement("type", true, null);
-                    addInsertHandler(element);
-                    resultSet.addElement(element);
-                    return;
-                }
-
-                // get list of all fields
-                List<String> luaTableFieldNames = new ArrayList<>();
-                List<LuaTableField> tableFieldList = table.getTableFieldList();
-                for (LuaTableField luaTableField : tableFieldList) {
-                    luaTableFieldNames.add(luaTableField.getName());
-                }
-
-                // get the className for this type
-                String typeText = type.getExprList().get(0).getFirstChild().getText();
-                typeText = typeText.replace("\"", "");
-                typeText = StringUtil.capitalizeWords(typeText, "-", true, false);
-                typeText = typeText.replace(" ", "");
-                typeText = "Prototype_" + typeText;
-
-                // get the correct Class, for this prototype
-                LuaClass color = LuaShortNamesManager.Companion.getInstance(project).findClass(typeText, searchContext);
-                if (color == null) {
-                    // Do nothing, when color not found
-                    return;
-                }
-                ITyClass colorType = color.getType();
-
-                // Iterate over all classes, this var derives from and print that completion
-                colorType.eachTopClass(tyClass -> {
-                    tyClass.lazyInit(searchContext);
-                    tyClass.processMembers(searchContext, (curType, member) -> {
-                        String memberName = member.getName();
-
-                        if (prefixMatcher.prefixMatches(memberName) && !luaTableFieldNames.contains(memberName)) {
-                            String className = curType.getDisplayName();
-                            if (member instanceof LuaClassField) {
-                                LuaLookupElement element = LookupElementFactory.Companion.createFieldLookupElement(className, memberName, ((LuaClassField) member), null, false);
-                                addInsertHandler(element);
-                                resultSet.addElement(PrioritizedLookupElement.withPriority(element, 15.0));
-                            }
-                        }
-                        return null;
-                    });
-                    return true;
-                });
+            ITy luaClass = FactorioPrototypeTypeGuesser.guessType(parameters.getPosition());
+            if (luaClass == null) {
+                // only complete member `type`
+                LuaLookupElement element = new LuaLookupElement("type", true, null);
+                addInsertHandler(element);
+                resultSet.addElement(element);
+                return;
             }
+
+            SearchContext searchContext = SearchContext.Companion.get(parameters.getPosition().getProject());
+
+            LuaTableExpr table = PsiTreeUtil.getParentOfType(parameters.getPosition(), LuaTableExpr.class);
+
+            // get list of all used fields
+            List<String> luaTableFieldNames = new ArrayList<>();
+            List<LuaTableField> tableFieldList = table.getTableFieldList();
+            for (LuaTableField luaTableField : tableFieldList) {
+                luaTableFieldNames.add(luaTableField.getName());
+            }
+
+            // Iterate over all classes, this var derives from and print that completion
+            luaClass.eachTopClass(tyClass -> {
+                tyClass.lazyInit(searchContext);
+                tyClass.processMembers(searchContext, (curType, member) -> {
+                    String memberName = member.getName();
+
+                    PrefixMatcher prefixMatcher = resultSet.getPrefixMatcher();
+                    if (prefixMatcher.prefixMatches(memberName) && !luaTableFieldNames.contains(memberName)) {
+                        String className = curType.getDisplayName();
+                        if (member instanceof LuaClassField) {
+                            LuaLookupElement element = LookupElementFactory.Companion.createFieldLookupElement(className, memberName, ((LuaClassField) member), null, false);
+                            addInsertHandler(element);
+                            resultSet.addElement(PrioritizedLookupElement.withPriority(element, 15.0));
+                        }
+                    }
+                    return null;
+                });
+                return true;
+            });
         }
     }
 
     /**
      * Add ` = ` to the autocompletion.
      * Also move the cursor to the correct position to just writer further.
-     *
-     * @// TODO: 14.12.19 add additional completion for different types
      *
      * @param element add the handler to
      */
@@ -107,9 +84,38 @@ public class FactorioPrototypeCompletionProvider extends CompletionProvider<Comp
             Document document = insertionContext.getDocument();
             Editor editor = insertionContext.getEditor();
 
+            SearchContext searchContext = SearchContext.Companion.get(insertionContext.getProject());
+
+            // Get Type
+            ITy type = ((LuaFieldLookupElement) lookupElement).getType();
+            type = TyAliasSubstitutor.Companion.substitute(type, searchContext);
+
             document.insertString(insertionContext.getTailOffset(), " = ");
-            document.insertString(insertionContext.getTailOffset(), "\"\"");
-            editor.getCaretModel().moveToOffset(insertionContext.getTailOffset() - 1);
+
+            int offsetRemoval = 0;
+
+            if (type.getKind().equals(TyKind.Array)) {
+                document.insertString(insertionContext.getTailOffset(), "{{}}");
+                offsetRemoval += 2;
+            } else if (type.getKind().equals(TyKind.Class)) {
+                document.insertString(insertionContext.getTailOffset(), "{}");
+                offsetRemoval += 1;
+            } else if (type.getKind().equals(TyKind.Primitive)) {
+                TyPrimitiveKind primitiveKind = null;
+                if (type instanceof TyPrimitive) {
+                    TyPrimitive tyPrimitive = (TyPrimitive) type;
+                    primitiveKind = tyPrimitive.getPrimitiveKind();
+                } else if (type instanceof TyPrimitiveClass) {
+                    TyPrimitiveClass tyPrimitiveClass = (TyPrimitiveClass) type;
+                    primitiveKind = tyPrimitiveClass.getPrimitiveKind();
+                }
+                if (primitiveKind != null && primitiveKind == TyPrimitiveKind.String) {
+                    document.insertString(insertionContext.getTailOffset(), "\"\"");
+                    offsetRemoval += 1;
+                }
+            }
+
+            editor.getCaretModel().moveToOffset(insertionContext.getTailOffset() - offsetRemoval);
             document.insertString(insertionContext.getTailOffset(), ",");
         });
     }
