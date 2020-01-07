b24d21fbe752ac91c05d3fedcca2f532828ba599
knoxfighter <knoxfighter@asdff45.de>
2020-01-07 17:27:19 +0100

    look for files based of project basePath when infer type

---
+++ src/main/java/moe/knox/factorio/library/fileResolver/FactorioRootDir.java
@@ -0,0 +1,25 @@
+package moe.knox.factorio.library.fileResolver;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import moe.knox.factorio.FactorioAutocompletionState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+
+public class FactorioRootDir extends FactorioFileResolver {
+    @Nullable
+    @Override
+    public VirtualFile find(@NotNull Project project, @NotNull String shortUrl, @NotNull String[] extNames) {
+        // Do nothing, if integration is deactivated
+        if (!FactorioAutocompletionState.getInstance(project).integrationActive) {
+            return null;
+        }
+
+        VirtualFile basePath = VfsUtil.findFileByIoFile(new File(project.getBasePath()), true);
+
+        return findFile(shortUrl, basePath, extNames);
+    }
+}
