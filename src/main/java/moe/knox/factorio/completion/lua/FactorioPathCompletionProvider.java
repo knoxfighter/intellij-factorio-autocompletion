0be0132ef5a75891767f28042a0718d5ebf79821
knoxfighter <knoxfighter@asdff45.de>
2020-01-09 01:48:02 +0100

    added autocompletion for lualib files

--- src/main/java/moe/knox/factorio/completion/lua/FactorioPathCompletionProvider.java
+++ src/main/java/moe/knox/factorio/completion/lua/FactorioPathCompletionProvider.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
 import com.tang.intellij.lua.lang.type.LuaString;
+import moe.knox.factorio.library.FactorioLualibParser;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.SystemIndependent;
 
@@ -23,6 +24,16 @@ public class FactorioPathCompletionProvider extends CompletionProvider<Completio
         // Path to the root of the project
         @SystemIndependent String projectPath = position.getProject().getBasePath();
 
+        iterateOverFiles(projectPath, position, resultSet);
+
+        // Path to the root of the Factorio Lualib
+        String currentLualibLink = FactorioLualibParser.getCurrentLualibLink(position.getProject());
+        if (currentLualibLink != null) {
+            iterateOverFiles(currentLualibLink, position, resultSet);
+        }
+    }
+
+    private void iterateOverFiles(String rootPath, PsiElement position, CompletionResultSet resultSet) {
         // Append the current subdirectories to project root
         String filePath = "";
         String currentLink = LuaString.Companion.getContent(position.getText()).getValue();
@@ -31,36 +42,39 @@ public class FactorioPathCompletionProvider extends CompletionProvider<Completio
             currentLink = currentLink.substring(0, lastDot);
             if (!currentLink.isEmpty()) {
                 currentLink = currentLink.replace(".", "/");
-                filePath = projectPath + "/" + currentLink;
+                filePath = rootPath + "/" + currentLink;
             }
         }
 
         if (filePath.isEmpty()) {
-            filePath = projectPath;
+            filePath = rootPath;
         }
 
         File file = new File(filePath);
 
-        // Iterate over all files in this directory (only show directories and lua files)
-        for (File listFile : file.listFiles(
-                file1 -> (file1.isDirectory() && !file1.getName().startsWith(".")) || getFileExtension(file1).equals("lua")
-        )) {
-            String relativePath = new File(projectPath).toURI().relativize(listFile.toURI()).getPath();
+        if (file.exists() && file.isDirectory()) {
 
-            // remove file extention
-            String fileExtension = getFileExtension(listFile);
-            if (!fileExtension.isEmpty()) {
-                relativePath = relativePath.substring(0, relativePath.indexOf(fileExtension) - 1);
+            // Iterate over all files in this directory (only show directories and lua files)
+            for (File listFile : file.listFiles(
+                    file1 -> (file1.isDirectory() && !file1.getName().startsWith(".")) || getFileExtension(file1).equals("lua")
+            )) {
+                String relativePath = new File(rootPath).toURI().relativize(listFile.toURI()).getPath();
+
+                // remove file extention
+                String fileExtension = getFileExtension(listFile);
+                if (!fileExtension.isEmpty()) {
+                    relativePath = relativePath.substring(0, relativePath.indexOf(fileExtension) - 1);
+                }
+                relativePath = relativePath.replace("/", ".");
+
+                LookupElementBuilder element = LookupElementBuilder.create(relativePath);
+                element = element.withInsertHandler((insertionContext, lookupElement) -> {
+                    Project project = insertionContext.getProject();
+                    Editor editor = insertionContext.getEditor();
+                    AutoPopupController.getInstance(project).autoPopupMemberLookup(editor, null);
+                });
+                resultSet.addElement(element);
             }
-            relativePath = relativePath.replace("/", ".");
-
-            LookupElementBuilder element = LookupElementBuilder.create(relativePath);
-            element = element.withInsertHandler((insertionContext, lookupElement) -> {
-                Project project = insertionContext.getProject();
-                Editor editor = insertionContext.getEditor();
-                AutoPopupController.getInstance(project).autoPopupMemberLookup(editor, null);
-            });
-            resultSet.addElement(element);
         }
     }
 
