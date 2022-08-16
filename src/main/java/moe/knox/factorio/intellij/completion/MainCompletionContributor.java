792ed522189c682400462edce8d361510a4400e3
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2022-08-16 20:27:26 +0300

    extract MainCompletionContributor subclasses

--- src/main/java/moe/knox/factorio/intellij/completion/MainCompletionContributor.java
+++ src/main/java/moe/knox/factorio/intellij/completion/MainCompletionContributor.java
@@ -1,15 +1,8 @@
 package moe.knox.factorio.intellij.completion;
 
 import com.intellij.codeInsight.completion.*;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.patterns.PatternCondition;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.ProcessingContext;
 import com.tang.intellij.lua.psi.*;
-import moe.knox.factorio.core.FactorioPrototypeState;
 import moe.knox.factorio.intellij.completion.factorio.*;
-import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
 
@@ -33,37 +26,12 @@ public class MainCompletionContributor extends CompletionContributor {
                         .with(new FactorioIntegrationActiveCondition(null))
                         .with(new PrototypePatternCondition(null, false))
                         .with(new PrototypeTypePatternCondition(null)),
-                new CompletionProvider<CompletionParameters>() {
-                    @Override
-                    protected void addCompletions(@NotNull CompletionParameters completionParameters, @NotNull ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet) {
-                        for (String prototypeType : FactorioPrototypeState.getInstance().getPrototypeTypes()) {
-                            completionResultSet.addElement(LookupElementBuilder.create(prototypeType));
-                        }
-                    }
-                });
+                new PrototypeTypeCompletionProvider());
 
         extend(CompletionType.BASIC,
                 psiElement(LuaTypes.STRING)
                         .with(new FactorioIntegrationActiveCondition(null))
-                        .with(new PatternCondition<PsiElement>(null) {
-                            @Override
-                            public boolean accepts(@NotNull PsiElement psiElement, ProcessingContext processingContext) {
-                                // Only run when string inside require
-                                LuaCallExpr callExpr = PsiTreeUtil.getParentOfType(psiElement, LuaCallExpr.class);
-
-                                if (callExpr != null) {
-                                    for (PsiElement callExprChild : callExpr.getChildren()) {
-                                        if (callExprChild instanceof LuaNameExpr) {
-                                            LuaNameExpr indexExpr = (LuaNameExpr) callExprChild;
-                                            if (indexExpr.getName().equals("require")) {
-                                                return true;
-                                            }
-                                        }
-                                    }
-                                }
-                                return false;
-                            }
-                        }),
+                        .with(new PathPatternCondition()),
                 new PathCompletionProvider()
         );
 
---
+++ src/main/java/moe/knox/factorio/intellij/completion/factorio/PathPatternCondition.java
@@ -0,0 +1,33 @@
+package moe.knox.factorio.intellij.completion.factorio;
+
+import com.intellij.patterns.PatternCondition;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.ProcessingContext;
+import com.tang.intellij.lua.psi.LuaCallExpr;
+import com.tang.intellij.lua.psi.LuaNameExpr;
+import org.jetbrains.annotations.NotNull;
+
+public class PathPatternCondition extends PatternCondition<PsiElement> {
+    public PathPatternCondition() {
+        super(null);
+    }
+
+    @Override
+    public boolean accepts(@NotNull PsiElement psiElement, ProcessingContext processingContext) {
+        // Only run when string inside require
+        LuaCallExpr callExpr = PsiTreeUtil.getParentOfType(psiElement, LuaCallExpr.class);
+
+        if (callExpr != null) {
+            for (PsiElement callExprChild : callExpr.getChildren()) {
+                if (callExprChild instanceof LuaNameExpr) {
+                    LuaNameExpr indexExpr = (LuaNameExpr) callExprChild;
+                    if (indexExpr.getName().equals("require")) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
