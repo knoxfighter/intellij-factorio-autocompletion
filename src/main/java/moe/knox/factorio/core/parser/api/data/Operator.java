0488784548644f186dac92ac15c416b7e763716b
Maxim Mironyuk <serieznyi@gmail.com>
2022-10-01 17:24:46 +0300

    Refactor Operator writing in lua

--- src/main/java/moe/knox/factorio/core/parser/api/data/Operator.java
+++ src/main/java/moe/knox/factorio/core/parser/api/data/Operator.java
@@ -3,20 +3,17 @@ package moe.knox.factorio.core.parser.api.data;
 import moe.knox.factorio.core.parser.api.data.JsonPolymorphism.JsonPolymorphism;
 import moe.knox.factorio.core.parser.api.data.JsonPolymorphism.JsonPolymorphismClass;
 
-
 @JsonPolymorphismClass("name")
 public class Operator implements Arrangeable {
-    /**
-     * "index", "length": Attributes
-     * "call": Method
-     */
-    public String name;
+    final public String name;
+    final public @JsonPolymorphism("call") Method method;
+    final public @JsonPolymorphism({"index", "length"}) Attribute attribute;
 
-    @JsonPolymorphism("call")
-    public Method method;
-
-    @JsonPolymorphism({"index", "length"})
-    public Attribute attribute;
+    public Operator(String name, Method method, Attribute attribute) {
+        this.name = name;
+        this.method = method;
+        this.attribute = attribute;
+    }
 
     public void arrangeElements() {
         if (method != null) {
@@ -25,6 +22,33 @@ public class Operator implements Arrangeable {
 
         if (attribute != null) {
             attribute.arrangeElements();
+        }
+    }
+
+    public Type getType()
+    {
+        return Type.fromNativeName(name);
+    }
+
+    public enum Type {
+        LENGTH("length"),
+        INDEX("index"),
+        CALL("call");
+
+        private final String nativeName;
+
+        Type(String nativeName) {
+            this.nativeName = nativeName;
+        }
+
+        public static Type fromNativeName(String nativeName) {
+            for (Type b : Type.values()) {
+                if (b.nativeName.equals(nativeName)) {
+                    return b;
+                }
+            }
+
+            return null;
         }
     }
 }
